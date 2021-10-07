58ffaa21ffd0beb509921e6a0ec4224da75a9812
Felipe Costa <felipecostasdc@gmail.com>
2021-10-07 17:51:12 -0300

    commit inicial

---
+++ app/src/main/java/com/example/processoseletivoletrasmusbr/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.processoseletivoletrasmusbr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.SearchView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/com/example/processoseletivoletrasmusbr/model/Nomes.kt
@@ -0,0 +1,6 @@
+package com.example.processoseletivoletrasmusbr.model
+
+// guarda o nome das musicas
+data class Nomes(val stringResourceId: Int) {
+
+}
